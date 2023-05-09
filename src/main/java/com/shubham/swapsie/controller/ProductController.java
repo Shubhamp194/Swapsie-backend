fdfd5403fc3d328e94148ebcbb0783b811bb696a
Shubhamp194 <shubhampandey194@gmail.com>
2023-05-10 00:11:11 +0530

    logging working

--- src/main/java/com/shubham/swapsie/controller/ProductController.java
+++ src/main/java/com/shubham/swapsie/controller/ProductController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
+@Slf4j
 @CrossOrigin("*")
 @RequestMapping("/product")
 public class ProductController {
@@ -18,46 +19,44 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    private static final Logger logger = LogManager.getLogger(ProductController.class);
 
     @PostMapping("/add")
     public ResponseEntity<Product> addProduct(@RequestBody Product product){
         Product newProduct = productService.addProduct(product);
-        logger.info("New Product Added");
+        log.info("New Product Added");
         return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
     }
 
     @GetMapping("getProducts")
     public ResponseEntity<List<Product>> getAllProducts(){
         List<Product> productList = productService.getAllProducts();
-        logger.info("All Products requested");
-        logger.error("eerere");
+        log.info("All Products requested");
         return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable long id){
         Product product = productService.getProductById(id);
-        logger.info("Specific Product requested having id : "+id);
+        log.info("Specific Product requested having id : "+id);
         return new ResponseEntity<>(product,HttpStatus.OK);
     }
 
     @GetMapping("/user/{user_id}")
     public ResponseEntity<List<Product>> getAllProductsBYUserId(@PathVariable long user_id){
         List<Product>  productList = productService.getAllProductsByUserId(user_id);
-        logger.info("All Products requested belonging to a particular user having userId : "+user_id);
+        log.info("All Products requested belonging to a particular user having userId : "+user_id);
         return new ResponseEntity<>(productList,HttpStatus.OK);
     }
     @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable long id){
-        logger.info("Product delete request initiated having productId : "+id);
+        log.info("Product delete request initiated having productId : "+id);
         return productService.deleteProduct(id);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable long id,@RequestBody Product product){
         Product updatedProduct = productService.updateProduct(id, product);
-        logger.info("Product update request initiated having productId : "+id);
+        log.info("Product update request initiated having productId : "+id);
         return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
     }
 
--- src/main/java/com/shubham/swapsie/controller/UserController.java
+++ src/main/java/com/shubham/swapsie/controller/UserController.java
@@ -7,12 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 @CrossOrigin("*")
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -20,46 +19,44 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    private static final Logger logger = LogManager.getLogger(UserController.class);
-
     @PostMapping("/register")
     public ResponseEntity<User> createUser(@RequestBody User user){
         User createdUser = userService.createUser(user);
-        logger.info("New user Added");
+        log.info("New user Added");
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
     @GetMapping("/getUsers")
     public ResponseEntity<List<User>> getUsers(){
         List<User> users = userService.getUsers();
-        logger.info("All Users List returned");
+        log.info("All Users List returned");
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable long id){
         User user = userService.getUserById(id);
-        logger.info("User Searched having id : "+id);
+        log.info("User Searched having id : "+id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable long id){
-        logger.info("User Removed having id : "+id);
+        log.info("User Removed having id : "+id);
         return userService.deleteUser(id);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<User> updateUser(@PathVariable long id,@RequestBody User updatedUser){
         User user = userService.updateUser(id, updatedUser);
-        logger.info("User Updated having id : "+id);
+        log.info("User Updated having id : "+id);
         return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @PostMapping("/login")
     public User login(@RequestBody LoginRequest loginRequest){
         User user = userService.login(loginRequest);
-        logger.info("User Login successful for user with email : "+user.getEmail());
+        log.info("User Login successful for user with email : "+user.getEmail());
         return user;
     }
 
